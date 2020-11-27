fbfe4340660d0e60eb9dd8a7835cb722122ff94b
Debra Wolford <debra.wolford@gmail.com>
2020-11-27 12:51:21 -0600

    Added waving emoji

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,18 @@
 // Collapsable Side Navbar
 $(document).ready(function () {
-  $(".side-nav-link").on("mouseover", function () {
+  $(".side-nav-link").mouseover(function () {
     $(this).siblings().toggleClass("d-none");
   });
 
-  $(".side-nav-link").on("mouseleave", function () {
+  $(".side-nav-link").mouseleave(function () {
     $(this).siblings().toggleClass("d-none");
+  });
+
+  $(".wiggle").mouseover(function () {
+    $(this).effect("shake", { times: 1 }, 500);
+  });
+
+  $(".wiggle").mouseleave(function () {
+    $(this).effect("none");
   });
 });
