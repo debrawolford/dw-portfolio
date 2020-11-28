5c44e52c49fd08da336b283db07599a084cbc737
Debra Wolford <debra.wolford@gmail.com>
2020-11-27 18:11:46 -0600

    Added portfolio section

--- static/js/script.js
+++ static/js/script.js
@@ -7,12 +7,4 @@ $(document).ready(function () {
   $(".side-nav-link").mouseleave(function () {
     $(this).siblings().toggleClass("d-none");
   });
-
-  $(".wiggle").mouseover(function () {
-    $(this).effect("shake", { times: 1 }, 500);
-  });
-
-  $(".wiggle").mouseleave(function () {
-    $(this).effect("none");
-  });
 });
