9c61ea904c3b96caab8dd6b3e4f1c364932668f1
Debra Wolford <debra.wolford@gmail.com>
2020-11-27 19:22:52 -0600

    added back to top function

--- static/js/script.js
+++ static/js/script.js
@@ -8,3 +8,25 @@ $(document).ready(function () {
     $(this).siblings().toggleClass("d-none");
   });
 });
+// Scroll to top function taken from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+//Get the button:
+button = document.getElementById("backToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
